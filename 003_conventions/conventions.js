0af582990ff9066b84b0384213b4f69b8a6e1a91
Hassan Al Bloushi <56361605+hasanq8net@users.noreply.github.com>
2023-12-13 23:27:23 +0300

    Create conventions.js

---
+++ 003_conventions/conventions.js
@@ -0,0 +1,63 @@
+const GLOBAL_VARIABLE = 20;
+const PI = Math.PI;
+
+/**
+ * function comment to define function
+ * @param 
+ * @return
+ */
+function myFirstFunctionInCamelCase() {
+  const myFirstVariable = 10;
+  console.log(PI);
+  console.log(myFirstVariable);
+
+  const x = 17;
+
+  /* if (x > 10) {
+    console.log('Greater than 10');
+  }
+  else {
+    console.log('Less than 10');
+  } */
+
+  const lettersOfTheAlphabet = ['a', 'b', 'c', 'd'];
+  console.log(lettersOfTheAlphabet);
+
+  const bike = {
+    type: 'road',
+    wheel: '700c',
+    color: 'red',
+    cost: 1299
+  };
+
+  // declare name variables
+  const firstName = 'Ben';
+  const lastName = "Collins";
+
+  // single-line comments
+
+  /*
+  multi-line comments
+  */
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
